ecde939829917d6e5bf3e4ce97b65088a578d88e
sid-thakur47 <sidthakur258@gmail.com>
2020-07-22 21:39:22 +0530

    ADDED:Functionality to take screenshots

--- BooksWagon1/Base/BooksWagonBase.cs
+++ BooksWagon1/Base/BooksWagonBase.cs
@@ -1,36 +1,89 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.MarkupUtils;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using System;
+using System.Net;
+using System.Net.Mail;
 using System.Threading;
 
 namespace BooksWagon1.Base
 {
-   public class BooksWagonBase
+    public class BooksWagonBase
     {
+        public static ExtentReports extent = ExtentReport.ReportManager.GetInstance();
         public static IWebDriver driver;
+        public static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static ExtentTest test;
+        public static int count = 1;
+
 
         [OneTimeSetUp]
         public void Initilize()
         {
-            driver = new FirefoxDriver();
-
+            driver = new ChromeDriver();
             //Using implicitly wait 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
             //Maximizing the window
             driver.Manage().Window.Maximize();
-
+            log.Info("Navigating to Login page");
             //Enter the url
             driver.Url = "https://www.bookswagon.com/login";
         }
 
-        [OneTimeTearDown]
-        public void Close()
-        {
-            Thread.Sleep(5000);
-            driver.Quit();
 
+
+        public static string TakeScreenshot(string testStatus)
+        {
+            count++;
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            Screenshot screenshot = ts.GetScreenshot();
+            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string finalPath = path.Substring(0, path.LastIndexOf("bin"))+"Screenshots\\" +testStatus+ ".png";
+            string localPath = new Uri(finalPath).LocalPath;
+            screenshot.SaveAsFile(localPath, ScreenshotImageFormat.Png);
+            return localPath;
+        }
+
+        public static void SendEmail(string email,string pass)
+        {
+            MailMessage mail = new MailMessage();
+            String fromEmail = email;
+            String password = pass;
+            String ToEmail = "sidthakur258@gmail.com";
+            mail.From = new MailAddress(fromEmail);
+            mail.To.Add(ToEmail);
+            mail.Priority = MailPriority.High;
+            mail.IsBodyHtml = true;
+            mail.Attachments.Add(new Attachment(@"C:\Users\Shivani\source\repos\BooksWagon\BooksWagon1\ExtentReport\index.html"));
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential(fromEmail, password);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+
+
+        [TearDown]
+        public void Close()        
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                string path = TakeScreenshot("FailedTest" + count);
+                test.AddScreenCaptureFromPath(path);
+                test.Fail(MarkupHelper.CreateLabel("Test Failed", ExtentColor.Red));
+            }
+            else if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed)
+            {
+                string path = TakeScreenshot("PassTest" + count);
+                test.AddScreenCaptureFromPath(path);
+                test.Pass(MarkupHelper.CreateLabel("Test Pass", ExtentColor.Green));
+            }
+            log.Info("Closing  browser");
+            Thread.Sleep(5000);
+            extent.Flush();
         }
     }
 }
--- BooksWagon1/Test/BooksWagonTest.cs
+++ BooksWagon1/Test/BooksWagonTest.cs
@@ -1,48 +1,90 @@
-﻿using Bookswagon.ExcelReader;
+﻿using AventStack.ExtentReports;
+using Bookswagon.ExcelReader;
 using Bookswagon.Pages;
 using BooksWagon1.Base;
 using BooksWagon1.Pages;
 using NUnit.Framework;
+using System.Threading;
 
 namespace Bookswagon.Test
 {
-   public class BooksWagonTest :BooksWagonBase
+    public class BooksWagonTest : BooksWagonBase
 
     {
+        public const string LOGIN_VALIDATE = "Personal Settings";
+        public const string HOME_VALIDATE = "Refine your Search";
+        public const string SHIPPING_VALIDATE = "Shipping Address";
+        public const string ORDER_VALIDATE = "Review your Order\r\nView Cart";
+
         Credentials credentials = new Credentials();
-       
-        [Test,Order(1)]
+
+        [Test, Order(1)]
         public void LoginTest()
         {
-            Login login = new Login(driver);
-            login.BookwagonLogin(credentials.userName, credentials.pass);
-            string title = driver.Title;
-            Assert.AreEqual(credentials.title, title);
-            //driver.Quit();
+                Login login = new Login(driver);
+                test = extent.CreateTest("Login Test");
+                test.Log(Status.Info, "Login Test started");
+                login.BookwagonLogin(credentials.userName, credentials.pass);
+                Assert.AreEqual(LOGIN_VALIDATE, login.Validate());
         }
 
-        [Test,Order(2)]
+        [Test, Order(2)]
         public void HomePageTest()
         {
+            test = extent.CreateTest("Homepage Test");
+            log.Info("Starting Homepage Test");
             Homepage home = new Homepage(driver);
             home.SearchItem("cobain");
+            Assert.AreEqual(HOME_VALIDATE, home.Validate());
         }
 
         [Test, Order(3)]
         public void PlaceOrderTest()
         {
+            test = extent.CreateTest("PlaceOrder Test");
+            log.Info("Placing order");
             PlaceOrder order = new PlaceOrder(driver);
             order.OrderBook();
+            Thread.Sleep(6000);
         }
 
+        [Test, Order(4)]
+        public void CartCheckoutTest()
 
-     /*   [Test, Order(4)]
+        {
+            test = extent.CreateTest("AddShipping Details Test");
+            log.Info("Adding shipping details");
+            ShippingAdress cart = new ShippingAdress(driver);
+            Assert.AreEqual(SHIPPING_VALIDATE, cart.Validate());
+            cart.AddShippingDetails("siddhesh", "ulwe", "India", "Maharashtra", "panvel", "410206", "8097155154");
+        }
+
+        [Test, Order(5)]
+        public void ReviewOrderTest()
+        {
+            test = extent.CreateTest("Review Order Test");
+            log.Info("Checking order");
+            ReviewOrder order = new ReviewOrder(driver);
+            Assert.AreEqual(ORDER_VALIDATE, order.Validate());
+            order.OrderReview();
+        }
+
+        [Test, Order(6)]
         public void LogoutTest()
         {
+            test = extent.CreateTest("Logout Test");
+            log.Info("Sign out from bookswagon");
             Login login = new Login(driver);
             login.Logout();
+        }
 
-        }*/
+        [OneTimeTearDown]
+        public void close()
+        {
+            driver.Quit();
+            SendEmail("sidthakur6433@gmail.com",credentials.ePass);
 
+
+        }
     }
 }
