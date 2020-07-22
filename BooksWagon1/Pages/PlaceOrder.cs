468080ba90121fc0d3ab2bb2c946f53dc22cd4ab
sid-thakur47 <sidthakur258@gmail.com>
2020-07-22 21:33:33 +0530

    REFACTORED:To correct verification of placing order

--- BooksWagon1/Pages/PlaceOrder.cs
+++ BooksWagon1/Pages/PlaceOrder.cs
@@ -1,22 +1,19 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
-using System;
 using System.Threading;
 
 namespace BooksWagon1.Pages
 {
     public class PlaceOrder
     {
+        public const string ORDER_VALIDATE = "Your order details will be sent to this email address.";
         public  IWebDriver driver;
         public PlaceOrder(IWebDriver driver)
         {
             this.driver = driver;
             PageFactory.InitElements(driver, this);
         }
-
-        [FindsBy(How = How.XPath, Using = "/html[1]/body[1]/form[1]/div[4]/div[3]/div[3]/div[2]/div[1]/div[4]/div[5]/a[1]/input[1]")]
-        public IWebElement buyNow;
 
         [FindsBy(How = How.XPath, Using = "//iframe[@class='cboxIframe']")]
         public IWebElement cartFrame;
@@ -27,9 +24,15 @@ namespace BooksWagon1.Pages
         [FindsBy(How = How.XPath, Using = "/html[1]/body[1]/form[1]/div[3]/div[3]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/a[1]")]
         public IWebElement continueButton;
 
+        [FindsBy(How = How.XPath, Using = "//p[contains(text(),'Your order details will be sent to this email addr')]")]
+        public IWebElement validation;
+
+        [FindsBy(How = How.XPath, Using = "/html[1]/body[1]/form[1]/div[4]/div[3]/div[3]/div[2]/div[1]/div[4]/div[5]/a[1]/input[1]")]
+        public IWebElement buyNow;
+
+
         public void OrderBook()
         {
-            Thread.Sleep(10000);
             buyNow.Click();
             Thread.Sleep(5000);
             driver.SwitchTo().Frame(cartFrame);
@@ -38,6 +41,7 @@ namespace BooksWagon1.Pages
             Thread.Sleep(5000);
             driver.SwitchTo().DefaultContent();
             Thread.Sleep(5000);
+            Assert.AreEqual(ORDER_VALIDATE, validation.Text);
             continueButton.Click();
         }
     }
