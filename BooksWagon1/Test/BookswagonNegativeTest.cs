2686b3ee40c400aa7e7a8f20c1c7d46da78162d5
sid-thakur47 <sidthakur258@gmail.com>
2020-07-24 16:43:16 +0530

    ADDED:Negative Test cases

---
+++ BooksWagon1/Test/BookswagonNegativeTest.cs
@@ -0,0 +1,30 @@
+﻿using Bookswagon.Pages;
+using BooksWagon1.Base;
+using NUnit.Framework;
+using static BooksWagon1.Utils.Utility;
+
+namespace BooksWagon1.Test
+{
+    [TestFixture]
+    [Parallelizable]
+   public class BookswagonNegativeTest : BooksWagonBase
+    {
+        public BookswagonNegativeTest(): base(Browser.FIREFOX)
+        {
+
+        }
+
+        [Test]
+        public void NegativeLoginTest()
+        {
+            Login login = new Login(driver);
+            login.BookwagonLogin("userName", "pass");
+        }
+
+        [OneTimeTearDown]
+        public void CLoseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
